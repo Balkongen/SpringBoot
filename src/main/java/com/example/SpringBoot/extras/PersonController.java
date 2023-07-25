b93cbc8ea3a139393b59aa62c7364498cc3e4e2d
Erik Olofsson <playde5555@gmail.com>
2023-07-25 23:47:02 +0200

    Added other http methods

--- src/main/java/com/example/SpringBoot/extras/PersonController.java
+++ src/main/java/com/example/SpringBoot/extras/PersonController.java
@@ -1,6 +1,11 @@
 package com.example.SpringBoot.extras;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +16,33 @@ import java.util.List;
 public class PersonController {
 
     @Autowired
-    PersonRepository personRepository;
+    private PersonRepository personRepository;
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+
+    private Person findPersonByName(String name) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("name").is(name));
+        return mongoTemplate.findOne(query, Person.class);
+    }
 
     @GetMapping("/show_all")
     public List<Person> getAllPersons() {
         return personRepository.findAll();
+    }
+
+    @GetMapping("/get_person/{name}")
+    public ResponseEntity<Person> getPerson(@PathVariable String name) {
+        Person person = findPersonByName(name);
+
+        if (person == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(person, HttpStatus.OK);
     }
 
     @GetMapping("/show_ages")
@@ -30,17 +56,31 @@ public class PersonController {
         return totAges;
     }
 
-    @RequestMapping(value = "/create", method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(value = "/createDefault", method = {RequestMethod.GET, RequestMethod.POST})
     public ResponseEntity<Person> addPerson() {
         try {
             Person person = new Person("Ed", 76);
             personRepository.save(person);
-            return new ResponseEntity<>(person, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(person, HttpStatus.OK);
         } catch (Exception e) {
             System.err.println("Error");
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
     }
 
+    @RequestMapping(value = "/createNew", produces = "application/json" ,method = {RequestMethod.GET, RequestMethod.POST})
+    public Person addPersonWithParameters(@RequestBody Person person) {
+        return personRepository.save(person);
+    }
+
+    @RequestMapping(value = "/delete/{name}", method = RequestMethod.DELETE)
+    public ResponseEntity<Person> deletePerson(@PathVariable String name) {
+        Person person = findPersonByName(name);
+
+        if (person == null) {
+           return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        personRepository.delete(person);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/SpringBoot/extras/PersonRepository.java
+++ src/main/java/com/example/SpringBoot/extras/PersonRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface PersonRepository extends MongoRepository<Person, String> {
 
+
+
 }
